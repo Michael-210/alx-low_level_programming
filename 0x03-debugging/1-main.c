d09b22fea0425d1379d4c030579e84be2c907588
Eyaife Michael <eyaifemichael18@gmail.com>
2022-09-14 13:04:55 -0700

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -24,13 +24,15 @@ int main(void)
 
 
 
-				       /*while (i < 10)
+				       /*
+					* while (i < 10)
 
 						        {
 
 								                putchar(i);
 
-										        }*/
+										        }
+											*/
 
 
 
