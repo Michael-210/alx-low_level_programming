a80bf2f88a35bfb84e194420b4a2dc6bba401851
Eyaife Michael <eyaifemichael18@gmail.com>
2022-09-15 09:22:13 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,6 +4,7 @@
  * main - program that prints either number
  * or fizz or buzz or fizzBuzz
  *
+ *
  * Return: 0
  */
 
@@ -15,19 +16,19 @@ int main(void)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 
 		else if ((num % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 
 		else if ((num % 5) == 0)
 		{
 			if (num != 100)
 			{
-				printf("Buzz");
+				printf("Buzz ");
 			}
 
 			else
