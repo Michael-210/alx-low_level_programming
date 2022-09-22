e1af3683e7195e7f2c602c9a52c5e58962b5f41a
Eyaife Michael <eyaifemichael18@gmail.com>
2022-09-22 10:57:32 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,7 @@ void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
+int _putchar(char c);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
