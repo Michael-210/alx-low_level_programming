2da75057f3212696397265b4db95837362d009e2
Eyaife Michael <eyaifemichael18@gmail.com>
2022-10-03 03:55:24 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	int i = 0, j;
 
 	if (width == 0 || height == 0)
-		return (null);
+		return (NULL);
 	array = (int **) malloc(sizeof(int *) * height);
 	if (array != NULL)
 	{
